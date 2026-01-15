231055463844c51c063d0a3bb419fdfee81ca147
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-01-15 22:17:18 +0530

    generate Raw Prompt based on FormData

--- app/create/components/PricingModel.jsx
+++ app/create/components/PricingModel.jsx
@@ -5,6 +5,7 @@ import Lookup from '@/app/_data/Lookup'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import { SignInButton, useUser } from '@clerk/nextjs'
+import Link from 'next/link'
 
 const PricingModel = ({onHandleInputChange,formData}) => {
   const {user} = useUser()
@@ -33,7 +34,10 @@ const PricingModel = ({onHandleInputChange,formData}) => {
                 ))}
                 </div>
                 {
-                  user?                <Button className='mt-5'>{pricing.button}</Button>:
+                  user?  
+                  <Link href={'/generate-logo?type-'+pricing.title}>              
+                      <Button className='mt-5'>{pricing.button}</Button>
+                  </Link>:
                   <SignInButton mode="modal" forceRedirectUrl={'/generate-logo?type='+pricing.title}>
                     <Button className='mt-5'>{pricing.button}</Button>
                   </SignInButton>
--- app/generate-logo/page.jsx
+++ app/generate-logo/page.jsx
@@ -1,13 +1,38 @@
 "use client"
+import { useState } from 'react';
 import { UserDetailContext } from '@/context/UserDetailContext'
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
+import Prompt from '../_data/Prompt';
 
 const GenerateLogo = () => {
   const {userDetail, setUserDetail} = useContext(UserDetailContext);
+  const [formData,setFormData]=useState()
+  useEffect(()=>{
+    if(typeof window!=='undefined' && userDetail?.email){
+      const storage=localStorage.getItem('formData');
+      if(storage){
+        setFormData(JSON.parse(storage));
+        console.log(JSON.parse(storage));
+      }
+    }
+  },[userDetail])
+  useEffect(()=>{
+   if(formData?.title){
+    GeneratedAILogo()
+   }
+  },[formData])
+  const GeneratedAILogo=()=>{
+    const PROMPT=Prompt.LOGO_PROMPT.replace('{logoTitle}',formData?.title)
+    .replace('{logoDesc}',formData?.desc)
+     .replace('{logoIdea}',formData.idea)
+    .replace('{logoColor}',formData.palette)
+    .replace('{logoDesign}',formData?.design?.title)
+    .replace('{logoPrompt}',formData?.design?.prompt)
+    console.log("prompt",PROMPT)
+  }
   return (
     <>
- 
-    <div>Generate Logo</div>
+     <div>Generate Logo</div>
     </>
   )
 }
